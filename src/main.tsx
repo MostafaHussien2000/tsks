6080214b11e3c87c2ccf85d26014e134ec341931
MostafaHussien2000 <mabozeed70@gmail.com>
2025-01-04 00:06:03 +0200

    reseting styles and defining variables

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 
+import "./styles/main.scss";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
